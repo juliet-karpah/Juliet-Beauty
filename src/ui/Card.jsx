4430598f40e5c21e407d73971bd133bcab3ea602
juliet-karpah <122952544+juliet-karpah@users.noreply.github.com>
2024-08-02 16:08:43 -0700

    add new design for product card

--- src/ui/Card.jsx
+++ src/ui/Card.jsx
@@ -3,9 +3,9 @@ import Rating from "./Rating";
 
 export default function Card(props) {
   return (
-    <div>
-      <Link to={`/product/${props.item}`}>
-        <img className="w-full object-cover h-full transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" src={props.src} alt={props.alt} />
+    <div className="">
+      <Link className="group block overflow-hidden" to={`/product/${props.item}`}>
+        <img className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]" src={props.src} alt={props.alt} />
       </Link>
       <div className="text-xl font-bold flex justify-between">
         <h3> {props.title}
